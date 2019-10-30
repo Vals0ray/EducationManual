4a825f70fe0c03d7aac563ad1066e4231c380d7f
Духнич Ярослав-Микола Сергійович <yaroslav-mykola.dukhnych.sa.2016@edu.lpnu.ua>
2019-10-30 20:27:08 +0200

    initial commit

---
+++ EducationManual/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿namespace EducationManual.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public int Age { get; set; }
+
+        public string Phone { get; set; }
+
+        public int? ClassroomId { get; set; }
+        public Classroom Classroom { get; set; }
+    }
+}
